1641cd568275afe853ab40a57735bb93293bd9b6
Akshay Sharma <31288779+aks30498@users.noreply.github.com>
2026-07-01 17:00:57 +0530

    Time: 218 ms (3.33%), Space: 64.6 MB (5.56%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    const res = Array.from({ length: n }, () => Array(n).fill(0));
+
+    const dc = [1,0,-1,0];
+    const dr = [0,1,0,-1];
+
+    let di = 0;
+
+    let r = 0;
+    let c = 0;
+
+    for(let i=1; i<=n*n; i++){
+        res[r][c] = i;
+
+        if(c+dc[di] >= n || c+dc[di] < 0 || r+dr[di] >= n || r+dr[di]<0 || res[r+dr[di]][c+dc[di]] !== 0) {
+            di = (di+1) % 4;
+        }
+        console.log(r, c, di, res)
+        r = r + dr[di];
+        c = c + dc[di];
+
+        
+    }
+
+    return res;
+};
