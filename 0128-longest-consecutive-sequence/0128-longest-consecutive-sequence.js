fcbd391b65af4672f262790eddf2d4c00a40d6aa
Akshay Sharma <31288779+aks30498@users.noreply.github.com>
2026-07-12 18:12:51 +0530

    Time: 42 ms (35.4%), Space: 80.9 MB (35.97%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,22 @@
+function longestConsecutive(nums) {
+    const numSet = new Set(nums);
+    let longestStreak = 0;
+
+    // FIX: Iterate over 'numSet' instead of 'nums' to eliminate duplicates
+    for (const num of numSet) {
+        
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+}
